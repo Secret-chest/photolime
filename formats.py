bdfba01000efba1cc52627b8e12e6cf852359871
vlad <vlad.muntoiu@gmail.com>
2023-04-11 11:34:44 +0300

    Save / open

---
+++ formats.py
@@ -0,0 +1,25 @@
+# read/write
+SUPPORTED = ["image/blp", "image/bmp", "image/dds",
+             "image/dib", "image/gif", "image/icns",
+             "image/x-icon", "image/im", "image/jpeg",
+             "image/pjpeg", "image/jp2", "image/jpm",
+             "image/msp", "image/x-citrix-jpeg",
+             "image/x-pcx", "image/png", "image/x-png",
+             "image/x-citrix-png",
+             "image/vnd.mozilla.apng", "image/apng",
+             "image/x-portable-bitmap",
+             "image/x-portable-graymap",
+             "image/x-portable-pixmap",
+             "image/x-portable-anymap",
+             "image/sgi", "image/spider",
+             "image/tga", "image/tiff", "image/webp"]
+# read-only
+READONLY  = ["image/cur", "image/dcx", "image/fits",
+             "video/x-fli", "image/vnd.fpx",
+             "image/ftex", "image/gbr", "image/imt",
+             "image/ipct", "image/naa", "image/mic",
+             "image/mpo", "image/pcd", "image/pixar",
+             "image/vnd.adobe.photoshop", "image/sun",
+             "image/wal"]
+# write-only
+WRITEONLY = ["image/palm", "application/pdf"]
--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import numpy
 from PIL import Image
+import formats
+import PIL
 import math
 import os
 import importlib
+import warnings
+warnings.filterwarnings("ignore")
 
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GdkPixbuf, GLib
 
+print(f"Running Pillow {PIL.__version__}")
+
 builder = Gtk.Builder()
 builder.add_from_file("main.glade")
 
 window = builder.get_object("root")
-window.show_all()
 
 operation = None
 
 zoomFactor = 1
+
+ZOOM_FACTOR = 0.25
 
 gtkImage = builder.get_object("image")
 zoomValue = builder.get_object("zoomValue")
@@ -24,10 +31,10 @@ zoomIndicator = builder.get_object("zoomIndicator")
 
 moduleList = builder.get_object("modules")
 
-# static = numpy.random.rand(512, 512, 3) * 256
-# image = Image.fromarray(static.astype("uint8")).convert("1").convert("RGB")
+static = numpy.random.rand(16, 16, 3) * 256
+image = Image.fromarray(static.astype("uint8")).convert("1").convert("RGB")
 
-image = Image.open("test.jpg")
+filePath = ""
 
 
 def render(event):
@@ -42,13 +49,135 @@ def render(event):
     gtkImage.set_from_pixbuf(pixbuf)
 
 
+def loadFile(path):
+    global image
+    image = Image.open(path)
+    render(None)
+
+
+def saveFile(path):
+    global image
+    image.save(path)
+
+
+def askForLoad(event):
+    global filePath
+
+    dialog = Gtk.FileChooserDialog(
+        title="Open File",
+        parent=window,
+        action=Gtk.FileChooserAction.OPEN,
+    )
+    dialog.add_buttons(
+        Gtk.STOCK_CANCEL,
+        Gtk.ResponseType.CANCEL,
+        Gtk.STOCK_OPEN,
+        Gtk.ResponseType.OK,
+    )
+
+    imageFilter = Gtk.FileFilter()
+    imageFilter.set_name("Supported images")
+    supported = formats.SUPPORTED + formats.READONLY
+    filters = []
+    for i in supported:
+        typeFilter = Gtk.FileFilter()
+        typeFilter.add_mime_type(i)
+        if i in formats.READONLY:
+            typeFilter.set_name("(read-only) " + i)
+        else:
+            typeFilter.set_name(i)
+        imageFilter.add_mime_type(i)
+        filters.append(typeFilter)
+    dialog.add_filter(imageFilter)
+
+    anyFilter = Gtk.FileFilter()
+    anyFilter.set_name("All files")
+    anyFilter.add_pattern("*")
+    dialog.add_filter(anyFilter)
+
+    for i in filters:
+        dialog.add_filter(i)
+
+    response = dialog.run()
+    if response == Gtk.ResponseType.OK:
+        filePath = dialog.get_filename()
+        print(f"Loading {filePath}")
+        loadFile(filePath)
+        dialog.destroy()
+        return filePath
+    else:
+        pass
+
+
+def askForSave(event):
+    dialog = Gtk.FileChooserDialog(
+        title="Save File",
+        parent=window,
+        action=Gtk.FileChooserAction.SAVE,
+    )
+    dialog.add_buttons(
+        Gtk.STOCK_CANCEL,
+        Gtk.ResponseType.CANCEL,
+        Gtk.STOCK_SAVE,
+        Gtk.ResponseType.OK,
+    )
+
+    imageFilter = Gtk.FileFilter()
+    imageFilter.set_name("Supported images")
+    supported = formats.SUPPORTED + formats.WRITEONLY
+    filters = []
+    for i in supported:
+        typeFilter = Gtk.FileFilter()
+        typeFilter.add_mime_type(i)
+        if i in formats.WRITEONLY:
+            typeFilter.set_name("(write-only) " + i)
+        else:
+            typeFilter.set_name(i)
+        imageFilter.add_mime_type(i)
+        filters.append(typeFilter)
+    dialog.add_filter(imageFilter)
+
+    anyFilter = Gtk.FileFilter()
+    anyFilter.set_name("All files")
+    anyFilter.add_pattern("*")
+    dialog.add_filter(anyFilter)
+
+    for i in filters:
+        dialog.add_filter(i)
+
+    response = dialog.run()
+    if response == Gtk.ResponseType.OK:
+        tempPath = dialog.get_filename()
+        print(f"Saving {tempPath}")
+        saveFile(tempPath)
+        dialog.destroy()
+        print(tempPath)
+        return tempPath
+    else:
+        pass
+
+def saveOver(event):
+    print(filePath)
+    saveFile(filePath)
+
+
+def saveAs(event):
+    global filePath
+    tempPath = askForSave(None)
+    if tempPath:
+        filePath = tempPath
+
+
+askForLoad(None)
+
+
 def zoomIn(event):
-    zoomValue.set_value(zoomValue.get_value() + 0.25)
+    zoomValue.set_value(zoomValue.get_value() + ZOOM_FACTOR)
     render(None)
 
 
 def zoomOut(event):
-    zoomValue.set_value(zoomValue.get_value() - 0.25)
+    zoomValue.set_value(zoomValue.get_value() - ZOOM_FACTOR)
     render(None)
 
 
@@ -74,7 +203,7 @@ def modifyImage():
         print("Getting new image")
         image = operation.image
         operation = None
-    render(None)
+        render(None)
     return True
 
 
@@ -106,8 +235,14 @@ handlers = {
     "zoom-out": zoomOut,
     "zoom-initial": zoomInitial,
     "zoom-fit": zoomFit,
+    "save": saveOver,
+    "save-as": saveAs,
+    "save-copy": askForSave,
+    "open": askForLoad,
 }
 builder.connect_signals(handlers)
+
+window.show_all()
 
 GLib.idle_add(modifyImage)
 
