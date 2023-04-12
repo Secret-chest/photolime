5e74fde9d7c5d2c43b1c7ed631c653c098ffc6da
vlad <vlad.muntoiu@gmail.com>
2023-04-12 10:35:57 +0300

    Make invert module work with new system

--- modules/invert/main.py
+++ modules/invert/main.py
@@ -1,29 +1,40 @@
 # Photolime metadata for GUI
 METADATA = {
-            "name": "Invert image",
-            "id": "invert",
-            "icon": ("GtkIcon", "image-invert-symbolic"),
-            "description": "Invert the image's RGB values"
-           }
+    "name": "Invert image",
+    "id": "invert",
+    "icon": ("GtkIcon", "image-invert-symbolic"),
+    "description": "Invert the image's RGB values"
+}
 
 import PIL
 import numpy
 from PIL import Image, ImageDraw
 
 
-def process(image: Image) -> Image:
-    w = image.width
-    h = image.height
+class Operation:
+    def __init__(self, image: Image):
+        self.image = image
+        self.ready = False
+        self.process()
 
-    pixels = image.load()
-    draw = ImageDraw.Draw(image)
+    def process(self) -> Image:
+        print("Modifying image!")
 
-    for x in range(w):
-        for y in range(h):
-            r, g, b = pixels[x, y]
-            r = 255 - r
-            g = 255 - g
-            b = 255 - b
-            draw.point((x, y), (r, g, b))
+        image = self.image
 
-    return image
+        w = image.width
+        h = image.height
+
+        pixels = image.load()
+        draw = ImageDraw.Draw(image)
+
+        for x in range(w):
+            for y in range(h):
+                r, g, b = pixels[x, y]
+                r = 255 - r
+                g = 255 - g
+                b = 255 - b
+                draw.point((x, y), (r, g, b))
+
+        self.image = image
+        self.ready = True
